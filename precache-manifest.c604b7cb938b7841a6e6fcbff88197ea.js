17bb0a44ac4a573d9dcfb0dab40c3f198440438e
Diogo Ortiz Machado <diogomachadors@gmail.com>
2019-07-29 10:34:52 -0300

    Updates

---
+++ precache-manifest.c604b7cb938b7841a6e6fcbff88197ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbab46b9689744e85a6ec9b989d23ba",
+    "url": "/calculator-reactjs/index.html"
+  },
+  {
+    "revision": "e22076d15129d6c2c224",
+    "url": "/calculator-reactjs/static/css/main.4c68c15b.chunk.css"
+  },
+  {
+    "revision": "75e14699f983e9cd6ff2",
+    "url": "/calculator-reactjs/static/js/2.ab87bc61.chunk.js"
+  },
+  {
+    "revision": "e22076d15129d6c2c224",
+    "url": "/calculator-reactjs/static/js/main.f9de93cc.chunk.js"
+  },
+  {
+    "revision": "a3475c4ae8dbba206d14",
+    "url": "/calculator-reactjs/static/js/runtime~main.ba0d8262.js"
+  }
+]);
