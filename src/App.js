de02b71066e875bec93d36047da0ca934e6a5eeb
Diogo Ortiz Machado <diogomachadors@gmail.com>
2019-07-29 09:42:33 -0300

    Added h1 with title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Calculator from './components/Calculator/Calculator';
 function App() {
   return (
     <div className="App">
+      <h1>Calculator-ReactJS</h1>
       <Calculator />
     </div>
   );
